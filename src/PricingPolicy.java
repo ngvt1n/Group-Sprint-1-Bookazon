195010e5ac84653a1187fd812acab7a34ce9dbb2
Rish C <104689301+AgentQuantum@users.noreply.github.com>
2025-09-29 19:38:49 -0400

    Update PricingPolicy.java

--- src/PricingPolicy.java
+++ src/PricingPolicy.java
@@ -4,9 +4,7 @@
  * Overloads avoid if/switch by using double dispatch.
  */
 public interface PricingPolicy {
-  double apply(double subtotal, NormalSubscription s);
-  double apply(double subtotal, SilverSubscription s);
-  double apply(double subtotal, GoldSubscription s);
-  double apply(double subtotal, PlatinumSubscription s);
+  double apply(double subtotal, Subscription s);
 }
+
 

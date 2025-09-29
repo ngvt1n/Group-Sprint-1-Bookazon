ae07df262958557c65bcdb1c7331754d7a34af40
Rish C <104689301+AgentQuantum@users.noreply.github.com>
2025-09-29 19:40:31 -0400

    Update NormalSubscription.java

--- src/NormalSubscription.java
+++ src/NormalSubscription.java
@@ -4,7 +4,15 @@
  */
 
 public class NormalSubscription implements Subscription {
-  @Override public String code() { return "normal"; }
+  private static final double PERCENT_OFF = Discounts.NORMAL_PERCENT_OFF;
+  private static final double FACTOR = Discounts.BASE_MULTIPLIER - PERCENT_OFF;
+
+  @Override public String code() { 
+    return "normal"; 
+  }
+  
   @Override public double price(double subtotal, PricingPolicy p) { return p.apply(subtotal, this); }
+  @Override public double factor() { return FACTOR; }
   @Override public String toString() { return code(); }
 }
+

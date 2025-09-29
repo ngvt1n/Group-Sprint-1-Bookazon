10739e85d706f2989f704d16d0b7f809bd413b9a
Rish C <104689301+AgentQuantum@users.noreply.github.com>
2025-09-29 19:39:58 -0400

    Update TablePricingPolicy.java

--- src/TablePricingPolicy.java
+++ src/TablePricingPolicy.java
@@ -9,23 +9,11 @@
  * 
  */
 public class TablePricingPolicy implements PricingPolicy {
-  private static final double FACTOR_NORMAL   = 1.00;
-  private static final double FACTOR_SILVER   = 0.95; // 5% off
-  private static final double FACTOR_GOLD     = 0.85; // 15% off
-  private static final double FACTOR_PLATINUM = 0.90; // 10% off
-
-  @Override public double apply(double subtotal, NormalSubscription   s) { 
-    return subtotal * FACTOR_NORMAL; 
-  }
-  @Override public double apply(double subtotal, SilverSubscription   s) { 
-    return subtotal * FACTOR_SILVER; 
-  }
-  @Override public double apply(double subtotal, GoldSubscription     s) { 
-    return subtotal * FACTOR_GOLD; 
-  }
-  @Override public double apply(double subtotal, PlatinumSubscription s) { 
-    return subtotal * FACTOR_PLATINUM; 
+  @Override
+  public double apply(double subtotal, Subscription s) {
+    return subtotal * s.factor();
   }
 }
+
 
 
